5b3d9e5e693dba304d36b70e6b2f00f246fbddf3
罗恒一 <1404814213@qq.com>
2018-04-04 18:39:22 +0800

    转移资产功能实现

---
+++ Application/Home/Controller/ShiftController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/4
+ * Time: 17:45
+ */
+
+namespace Home\Controller;
+
+
+class ShiftController
+{
+
+}
---
+++ Application/Otc/Model/UserCoinModel.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/4
+ * Time: 18:03
+ */
+
+namespace Otc\Model;
+
+
+class UserCoinModel
+{
+
+}
